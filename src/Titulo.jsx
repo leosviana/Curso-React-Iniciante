75b50b0a84e329e48c3c0909e48669d3e782c490
Leonardo S. Viana <leosiqueiraviana@gmail.com>
2024-04-25 14:34:55 -0300

    Update Titulo.jsx

--- src/Titulo.jsx
+++ src/Titulo.jsx
@@ -1,11 +1,12 @@
-function Titulo(){
+function Titulo({amigo, cor}){ /* Recuperando o PROPs "nome" */
     let nome = "Leonardo Viana";
     const soma = 10 + 10;
     const urlImg = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/React_Logo_SVG.svg/120px-React_Logo_SVG.svg.png";
     return (
         <div>
-            <h1>Olá eu sou um título!</h1>   {/* Texto simples */}
+            <h1 style={{color: cor}}>Olá eu sou um título!</h1>   {/* Texto simples */}
             <p>Me chamo {nome}</p>           {/* Texto com interpolação de variável string */}
+            <p>Meu amigo se chama {amigo ? amigo : "Beltrano"}</p>
             <p>Soma: 10 + 10 = {soma}</p>    {/* Texto com interpolação de variável numérica */}
             <img width={100} src={urlImg} /> {/* Imagem com interpolação de variável de imagem */}
         </div>
